29ab4a94f77b86cb78dac69c456c8808afd6d82c
DrZin-dev <pasawee.dev@gmail.com>
2019-08-22 17:02:57 +0700

    commit

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -34,6 +34,7 @@
 #include <env.h>
 
 //* Prototype
+void wifiSetup();
 
 //* Server Details
 #define MQTT_SERVER
@@ -42,9 +43,14 @@
 #define MQTT_PASSWORD
 #define MQTT_NAME
 
+//* MQTT Topic
+#define MQTT_SUB_TEST "/NODE_RED/Test" //? Test MQTT Only
+#define MQTT_SUB_JSON "/NODE_RED/JSON" //? JSON Format
+#define MQTT_PUB_JSON "/ESP32_1/JSON_PUB"
+
 //* Wifi Details
-#define WIFI_STA_NAME
-#define WIFI_STA_PASSWORD
+#define WIFI_STA_NAME MY_WIFI_USER
+#define WIFI_STA_PASSWORD MY_WIFI_PASSWORD
 
 //! Storage Params
 const char **title;
@@ -60,15 +66,46 @@ int **pressure;
 char buffer[200];
 
 //* Basic Params
-
+unsigned long previousMillis_WiFi = 0, previousMillis_MQTT = 0;
+#define Intervals 500
 //* Object && Create Connection
 
 //* Addition Function
+void wifiSetup()
+{
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_STA_NAME);
+
+  //* WiFi Connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_STA_NAME, WIFI_STA_PASSWORD);
+
+  Serial.print("Status: [");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    //* Time Loop Uss Millis
+    unsigned long currentMillis = millis(); //? Store Current time
+    if (currentMillis - previousMillis_WiFi > Intervals)
+    {
+      previousMillis_WiFi = currentMillis; //? Save Current time
+      Serial.print("=");
+    }
+  }
+  Serial.println("] 100%");
+
+  Serial.println("--- WiFi Connected ---");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println();
+  wifiSetup();
 }
 
 void loop()
