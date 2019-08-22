b1b559a7fa1c4ad55062bc860f12e0a6583ba128
DrZin-dev <pasawee.dev@gmail.com>
2019-08-22 21:17:34 +0700

    Real time Update

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -44,6 +44,14 @@ void pubJSON();
 void pubRequest();
 void todoTopic(byte *payload, unsigned int length);
 void wifiSetup();
+void Loading();
+
+//* Display Prototype
+void text();
+void menu();
+void setCursorText(int x, int y);
+void Todo();
+void Sleep();
 
 //* Server Details
 #define MQTT_SERVER MY_MQTT_SERVER
@@ -74,6 +82,15 @@ unsigned long previousMillis_WiFi = 0, previousMillis_MQTT = 0, previousMillis_P
 int count = 0;
 int cur_count = 0;
 bool isFetch = false;
+
+//* Display Params
+const unsigned int GREENTYPE = M5.Lcd.color565(7, 235, 235);
+const unsigned int GREY = M5.Lcd.color565(153, 153, 153);
+int state = 0;
+int stateMenuL = 0;
+int stateMenuR = 0;
+int stateMenuTD = 0;
+int stateMenuSL = 0;
 
 //* Object && Create Connection
 WiFiClient client;         //? Create TCP Connection
@@ -118,6 +135,7 @@ void mqttSetup()
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
+
   payload[length] = '\0';
   String topic_str = topic, payload_str = (char *)payload;
   Serial.println("[" + topic_str + "]: " + payload_str);
@@ -208,6 +226,7 @@ void countTopic(String payload)
     count = payload.toInt();
     cur_count = 0;
     isFetch = false;
+    Loading();
     memset(str_title, 0, sizeof(str_title));
     memset(str_label, 0, sizeof(str_label));
     memset(str_date, 0, sizeof(str_date));
@@ -251,6 +270,7 @@ void todoTopic(byte *payload, unsigned int length)
   if (isFetch)
   {
     printStorage();
+    menu();
   }
 }
 
@@ -269,15 +289,152 @@ void printStorage()
     i++;
   }
 }
+
+void text()
+{
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2.5);
+}
+void setCursorText(int x, int y)
+{
+  M5.Lcd.setCursor(x, y);
+}
+void menu()
+{
+
+  M5.Lcd.fillScreen(GREENTYPE);
+  M5.Lcd.fillCircle(100, 100, 20, GREY);
+  M5.Lcd.fillCircle(200, 100, 20, GREY);
+  M5.Lcd.fillCircle(112, 90, 10, WHITE);
+  M5.Lcd.fillCircle(212, 90, 10, WHITE);
+  text();
+  setCursorText(130, 200);
+  M5.Lcd.printf("Todo");
+  //***********************************
+  while (1)
+  {
+    if (M5.BtnB.wasPressed())
+    {
+      state++;
+    }
+    if (state == 1)
+    {
+      state = 0;
+      Todo();
+    }
+    else
+    {
+      state = 0;
+    }
+    mqtt.loop();
+    M5.update();
+  }
+}
+void Todo()
+{
+  text();
+  M5.Lcd.fillScreen(GREENTYPE);
+  M5.Lcd.fillRect(0, 0, 900, 50, GREY);
+  setCursorText(100, 20);
+  M5.Lcd.print("Todo List");
+  setCursorText(250, 200);
+  M5.Lcd.printf("Sleep");
+
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(3);
+  setCursorText(0, 70);
+  int i_count = count;
+  if (i_count > 6)
+  {
+    i_count = 6;
+  }
+  for (int x = 0; x < i_count; x++)
+  {
+    // String status_sym = str_status_[x].equals("Todo") ? " " : "/";
+    // M5.Lcd.print(" [" + status_sym + "] ");
+
+    M5.lcd.print(str_title[x]);
+    M5.Lcd.print(" [" + str_label[x] + "]");
+    M5.lcd.println();
+  }
+
+  while (1)
+  {
+
+    if (M5.BtnC.wasPressed())
+    {
+      state++;
+    }
+    if (state == 1)
+    {
+      state = 0;
+      Sleep();
+    }
+    else
+    {
+      state = 0;
+    }
+    mqtt.loop();
+    M5.update();
+  }
+}
+
+void Sleep()
+{
+  text();
+  M5.Lcd.fillScreen(GREENTYPE);
+  M5.Lcd.fillRect(70, 100, 50, 10, GREY);
+  M5.Lcd.fillRect(190, 100, 50, 10, GREY);
+  setCursorText(45, 200);
+  M5.Lcd.printf("Home");
+  while (1)
+  {
+    if (M5.BtnA.wasPressed())
+    {
+      state++;
+    }
+    if (state == 1)
+    {
+      state = 0;
+      menu();
+    }
+    else
+    {
+      state = 0;
+    }
+    mqtt.loop();
+    M5.update();
+  }
+}
+
+void Loading()
+{
+  M5.Lcd.clearDisplay();
+  M5.Lcd.fillScreen(GREENTYPE);
+  M5.Lcd.fillCircle(100, 100, 20, GREY);
+  M5.Lcd.fillCircle(200, 100, 20, GREY);
+  M5.Lcd.fillCircle(112, 90, 10, WHITE);
+  M5.Lcd.fillCircle(212, 90, 10, WHITE);
+  text();
+  setCursorText(110, 150);
+  M5.Lcd.printf("Loading...");
+  M5.update();
+  //***********************************
+}
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
+  M5.begin();
+  Loading();
   wifiSetup();
   mqttSetup();
 }
 
+void pubENV()
+{
+}
 void loop()
 {
   // put your main code here, to run repeatedly:
@@ -288,6 +445,13 @@ void loop()
   }
   else
   {
+
     mqtt.loop();
+  }
+  if (isFetch)
+  {
+    M5.Lcd.clearDisplay();
+    M5.update();
+    menu();
   }
 }
